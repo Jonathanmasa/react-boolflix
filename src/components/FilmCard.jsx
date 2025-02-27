5260b3469586e1d5044c4fd3b78529d60329d273
Jonathanmasa <jonathanmasa97@gmail.com>
2025-02-27 19:31:42 +0100

    feat[flag-language]

--- src/components/FilmCard.jsx
+++ src/components/FilmCard.jsx
@@ -1,17 +1,52 @@
 const fallbackImage = "https://via.placeholder.com/300x750?text=No+Image";
 
+// Mappa per lingue comuni 
+const languageFlagMap = {
+  'en': 'us',   
+  'es': 'es',   
+  'fr': 'fr',  
+  'it': 'it',   
+
+};
+
+// Funzione per ottenere l'URL della bandiera in base al codice lingua
+const getFlagUrl = (languageCode) => {
+  // Uso il codice mappato o il codice lingua stesso
+  const flagCode = languageFlagMap[languageCode] || languageCode; 
+  return `https://flagcdn.com/w20/${flagCode.toLowerCase()}.png`;
+};
+
 function FilmCard(props) {
   const { movie } = props;
   const imageUrl = movie.poster_path 
     ? `https://image.tmdb.org/t/p/w500${movie.poster_path}` 
     : fallbackImage;
 
+  const languageCode = movie.original_language ? movie.original_language.toLowerCase() : null;
+  const flagUrl = languageCode ? getFlagUrl(languageCode) : null;
+
   return (
     <div className="film-card">
       <img src={imageUrl} alt={movie.title} />
       <h2>{movie.title || "Titolo non disponibile"}</h2>
       <h4>Titolo Originale: {movie.original_title || "Non disponibile"}</h4>
-      <p>Lingua: {movie.original_language ? movie.original_language.toUpperCase() : "Lingua non disponibile"}</p>
+      <div className="language-info">
+        <p>
+          Lingua: 
+          {languageCode ? (
+            <span>
+              <img 
+                src={flagUrl} 
+                alt={`Bandiera ${movie.original_language}`} 
+                style={{ width: "20px", marginRight: "5px" }} 
+              />
+              {movie.original_language.toUpperCase()}
+            </span>
+          ) : (
+            "Lingua non disponibile"
+          )}
+        </p>
+      </div>
       <p>Voto: {movie.vote_average ? movie.vote_average : "Voto non disponibile"}</p>
     </div>
   );
